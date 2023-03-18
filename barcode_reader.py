9262b9fbc1fa7cc0d77afbae71ddf89a256bb955
Nguyễn Tuấn Anh <115028201+Ng-Tuan-Anh@users.noreply.github.com>
2023-03-18 16:03:30 +0700

    Update barcode_reader.py

--- barcode_reader.py
+++ barcode_reader.py
@@ -3,7 +3,7 @@ import cv2
 from pyzbar import pyzbar 
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-model = torch.hub.load('ultralytics/yolov5', 'custom',path='barcode_detection.pt').to(device)
+model = torch.hub.load('ultralytics/yolov5', 'custom',path='model.pt').to(device)
 
 cap = cv2.VideoCapture(1)
 while cap.isOpened():
